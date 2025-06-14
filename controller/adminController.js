01a8b918cda5097e2c8bb18e077ffcf856576d5a
williamchidi <williamchidi1@gmail.com>
2025-06-14 13:00:45 +0100

    updated

--- controller/adminController.js
+++ controller/adminController.js
@@ -15,7 +15,7 @@ const signToken = (id, res) => {
     httpOnly: true,
     sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
